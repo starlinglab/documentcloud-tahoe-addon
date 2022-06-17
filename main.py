0e696ddbad3e395ac6146feeb4a55bd57991bc9a
makeworld <makeworld@protonmail.com>
2022-06-17 09:55:34 -0400

    Upload files

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import os
 import requests
 from documentcloud.addon import AddOn
 
-SERVER = "https://example.com"  # No ending slash
+SERVER = "https://tahoe.starlinglab.org"  # No ending slash
 PASSWORD = os.environ["TAHOE_PASSWORD"]  # For server basicauth
+TAHOE_DIR = os.environ["TAHOE_DIR"].replace(":", "%3A")  # Root dircap, URL encoded
 
 
 class Tahoe(AddOn):
     def main(self):
         for doc_id in self.documents:
-            pass
+            document = self.client.documents.get(doc_id)
+            r = requests.put(
+                f"{SERVER}/uri/{TAHOE_DIR}/{str(document.id)}-{document.slug}.pdf",
+                auth=("user", PASSWORD),
+                timeout=10,
+            )
+            if r.status_code not in [200, 201]:
+                self.set_message(f"Uploading failed with status code {r.status_code}")
+                return
+            filecap = r.text.strip()
+            document.data["tahoe-filecap"] = filecap
+            document.save()
+            self.set_message(f"Upload complete: {filecap}")
 
 
 if __name__ == "__main__":
